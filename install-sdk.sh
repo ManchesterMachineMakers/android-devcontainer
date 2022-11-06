a713bf26d9eb3cce1e2d73da2929086728e7ed4c
Aleks Rūtiņš <aleks@rutins.com>
2022-11-06 08:06:04 -0500

    Install JDK using apt

--- install-sdk.sh
+++ install-sdk.sh
@@ -3,6 +3,6 @@ curl -s "https://get.sdkman.io" | bash
 chmod a+x "$HOME/.sdkman/bin/sdkman-init.sh"
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-sdk install java 17-open
+sudo apt install openjdk-17-jdk
 sdk install kotlin
 sdk install gradle
